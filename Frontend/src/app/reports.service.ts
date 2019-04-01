9025630e6f674317495bc1dbf7501a7567325f7d
Abdelmlak Dhif <dhifmlak@gmail.com>
2019-04-01 17:03:30 +0100

    report fix

--- Frontend/src/app/reports.service.ts
+++ Frontend/src/app/reports.service.ts
@@ -8,7 +8,6 @@ export class ReportsService {
   constructor(private http: HttpClient) {}
 
   uploadReport(file) {
-    //const headers = new HttpHeaders().set('Content-Type', 'multipart/form-data');
     return this.http.post(`http://localhost:3000/reports/new`, file);
   }
 
